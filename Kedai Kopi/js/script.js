ede229f847f6f62b30bc6399df9034ff920b70e5
AMTATO <ilhampradipta12@gmail.com>
2023-11-27 18:33:52 +0700

    Latihan membuat website kedai kopi part 10

--- Kedai Kopi/js/script.js
+++ Kedai Kopi/js/script.js
@@ -1,16 +1,67 @@
-// Toogle class active
+// Toogle class active untuk hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
 
 // Ketika tombol hamburger di klik
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
+  e.preventDefault();
 };
 
-// Klik di luar sidebar untuk menghilangkan nav
-const hamburger = document.querySelector("#hamburger-menu");
+// Toogle class active untuk search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+// Ketika tombol search-button di klik
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// Toogle class active untuk shopping cart
+const shoppingCart = document.querySelector(".shopping-cart");
+
+// Ketika tombol shopping-cart-button di klik
+document.querySelector("#shopping-cart-button").onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
+};
+
+// Klik di luar elemen untuk menghilangkan nav, search box, shopping cart
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
+const scb = document.querySelector("#shopping-cart-button");
 
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+  if (!scb.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
+  }
 });
+
+// Modal Box
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailButtons = document.querySelectorAll(".item-detail-button");
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = "flex";
+    e.preventDefault();
+  };
+});
+
+// Klik tombol close modal
+document.querySelector(".modal .close-icon").onclick = (e) => {
+  itemDetailModal.style.display = "none";
+  e.preventDefault();
+};
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = "none";
+  }
+};
